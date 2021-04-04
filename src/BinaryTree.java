ca2f40d6c62aece2aa013937bb658ff37c28218a
Artis <mundurs.artis@gmail.com>
2021-04-04 17:34:54 +0200

    Update BinaryTree.java

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -3,6 +3,7 @@ import jdk.nashorn.api.tree.ArrayLiteralTree;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 
 public abstract class BinaryTree {
     private BinaryTreeNode root;
@@ -47,61 +48,135 @@ public abstract class BinaryTree {
 
     public boolean contains(int element) {
 
-        if(root == null) return false;
-        if(root.getElement() == element) return true;
+        if (root == null) return false;
+        if (root.getElement() == element) return true;
         Queue<BinaryTreeNode> q = new LinkedList<>();
         q.offer(root);
 
         while (!q.isEmpty()) {
             BinaryTreeNode node = q.poll();
-            if (node.getLeftChild() != null)
-            {
-                if(node.getElement() == element) return true;
+            if (node.getLeftChild() != null) {
+                if (node.getElement() == element) return true;
                 q.offer(node.getLeftChild());
             }
 
-            if (node.getRightChild() != null)
-            {
-                if(node.getElement() == element) return true;
+            if (node.getRightChild() != null) {
+                if (node.getElement() == element) return true;
                 q.offer(node.getRightChild());
             }
         }
         return false;
     }
 
-    public ArrayList<BinaryTreeNode> inOrder()
-    {
-        if(isEmpty()) return null;
+    public ArrayList<BinaryTreeNode> inOrder() {
 
+        if (isEmpty()) return null;
+        Queue<BinaryTreeNode> q = new LinkedList<>();
+        ArrayList<BinaryTreeNode> finalTraversal = new ArrayList<>();
+        BinaryTreeNode current = root;
+        q.offer(current);
+
+        while (!q.isEmpty() || current != null) {
+            if(current != null)
+            {
+                q.offer(current);
+                current = current.getLeftChild();
+            }
+            else
+            {
+                finalTraversal.add(q.poll());
+                current = current.getRightChild();
+            }
+        }
+        return finalTraversal;
     }
 
     public ArrayList<BinaryTreeNode> preOrder()
     {
         if(isEmpty()) return null;
 
-        Queue<BinaryTreeNode> q = new LinkedList<>();
-        q.offer(root);
+        Stack<BinaryTreeNode> q = new Stack<BinaryTreeNode>();
+        q.push(root);
 
         ArrayList<BinaryTreeNode> preList = new ArrayList<>();
-        preList.add(root);
 
         while (!q.isEmpty())
         {
-            BinaryTreeNode node = q.poll();
-            int index = preList.indexOf(node);
+            BinaryTreeNode node = q.pop();
+            preList.add(node);
 
             if (node.getRightChild() != null)
             {
-                preList.add(2*(index + 1), node.getRightChild());
-                q.offer(node.getRightChild());
+                q.push(node.getRightChild());
             }
             if (node.getLeftChild() != null)
             {
-                preList.add(2*index + 1, node.getLeftChild());
-                q.offer(node.getLeftChild());
+                q.push(node.getLeftChild());
             }
         }
 
         return preList;
+    }
+
+    public ArrayList<BinaryTreeNode> levelOrder()
+    {
+        if(isEmpty()) return null;
+
+        Queue<BinaryTreeNode> q = new LinkedList<>();
+        q.offer(root);
+
+        ArrayList<BinaryTreeNode> levelOrder = new ArrayList<>();
+        levelOrder.add(root);
+
+        while (!q.isEmpty())
+        {
+            BinaryTreeNode node = q.poll();
+            int index = levelOrder.indexOf(node);
+
+            if (node.getRightChild() != null)
+            {
+                levelOrder.add(2(index + 1), node.getRightChild());
+                q.offer(node.getRightChild());
+            }
+            if (node.getLeftChild() != null)
+            {
+                levelOrder.add(2index + 1, node.getLeftChild());
+                q.offer(node.getLeftChild());
+            }
+        }
+
+        return levelOrder;
+    }
+
+    public ArrayList<BinaryTreeNode> postOrder() {
+
+        if (root == null) return null;
+        ArrayList<BinaryTreeNode> postList = new ArrayList<>();
+
+
+        Stack stack1 = new Stack<>();
+        Stack stack2 = new Stack<>();
+
+
+        stack1.push(root);
+
+
+        while (!stack1.isEmpty()) {
+
+            BinaryTreeNode temp = (BinaryTreeNode) stack1.pop();
+            stack2.push(temp);
+
+            if (temp.getLeftChild() != null)
+                stack1.push(temp.getLeftChild());
+            if (temp.getRightChild() != null)
+                stack1.push(temp.getRightChild());
+        }
+
+
+        while (!stack2.isEmpty()) {
+            BinaryTreeNode temp = (BinaryTreeNode) stack2.pop();
+            postList.add(temp);
+        }
+        return postList;
     }
 }
